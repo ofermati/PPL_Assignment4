7d82ab943645800f83d069cc34ea840ed89bd7b9
ofer <ofermati@post.bgu.ac.il>
2025-06-15 14:09:30 +0300

    q1 complete

--- ex4.ts
+++ ex4.ts
@@ -1,12 +1,26 @@
 //Q1
-export function all<T>(promises : Array<Promise<T>>) : Promise<Array<T>> {
+export function all<T>(promises: Array<Promise<T>>): Promise<T[]> {
+  return new Promise<T[]>((resolve, reject) => {
+    const n = promises.length;
+    const results: T[] = new Array(n);
+    let remaining = n;
 
-  return new Promise<T[]>( (resolve, reject) => {
-    resolve([]);
-    //TODO
+    if (n === 0) {
+      resolve([]);
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      promises[i]           
+        .then(value => {
+          results[i] = value;  
+          if (--remaining === 0) 
+            resolve(results); 
+        })
+        .catch(reject);
+    }
   });
 }
-
   
 // Q2
 export function* Fib1() {
