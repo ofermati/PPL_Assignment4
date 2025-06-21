2397ab76d5fcc2dc562e107e06c47f868fed2ad5
Nitzan Dudai <Nitzandudai@gmail.com>
2025-06-21 12:19:25 +0300

    פוש ליתר ביטחון

--- ex4.ts
+++ ex4.ts
@@ -24,12 +24,30 @@ export function all<T>(promises: Array<Promise<T>>): Promise<T[]> {
   
 // Q2
 export function* Fib1() {
-	// @TODO
-  return undefined;
+  let prev = 1;
+  yield prev;
+
+  let curr = 1;
+  yield curr;
+
+  while(true){
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+    yield curr;
+  }
 }
 
 
 export function* Fib2() {
-	// @TODO
-  return undefined;
+  const sqrt5 = Math.sqrt(5);
+  const phi   = (1 + sqrt5) / 2;
+  const psi   = (1 - sqrt5) / 2;   
+  let n = 1;                      
+  while (true) {
+    const curr = Math.round((phi ** n - psi ** n) / sqrt5);
+    yield curr;   
+    n++;          
+  }
 }
+
